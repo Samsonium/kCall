678ddd0240579bb059b4312e14d8824dcd2fbe8f
Samsonium <tdesu@vk.com>
2023-08-18 16:08:29 +0300

    Finish main script

---
+++ back/src/main.ts
@@ -0,0 +1,10 @@
+import KCallServer from './server';
+import KCallSocket from './socket';
+
+function main() {
+    const kserv = new KCallServer();
+    const ksock = new KCallSocket(kserv.server);
+
+    ksock.setup();
+    kserv.server.listen(7000);
+}
