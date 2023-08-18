1169e993b33e7c9ee134a66c401366b3b209845b
Samsonium <tdesu@vk.com>
2023-08-18 17:25:48 +0300

    Remove self-starting server inside test file

--- back/test/room.spec.ts
+++ back/test/room.spec.ts
@@ -1,28 +1,13 @@
-import {describe, it, expect, beforeAll, afterAll} from 'vitest';
+import {describe, it, expect} from 'vitest';
 import {default as io, Socket} from 'socket.io-client';
 import SocketInMethods from '../src/socket/SocketInMethods';
 import SocketOutMethods from '../src/socket/SocketOutMethods';
-import KCallServer from '../src/server';
-import KCallSocket from '../src/socket';
-
-let serv: KCallServer;
-let sock: KCallSocket;
-beforeAll(() => {
-    serv = new KCallServer();
-    sock = new KCallSocket(serv.server);
-    sock.setup();
-    serv.server.listen(7001);
-});
-afterAll(() => {
-    sock.io.close();
-    serv.server.close();
-})
 
 describe('Room join and leave', () => {
     let socket: Socket<SocketInMethods, SocketOutMethods>;
 
     it('Connect', () => {
-        socket = io('ws://localhost:7001');
+        socket = io('ws://localhost:7000');
         expect(socket).toBeInstanceOf(Socket);
     })
     it('Join room', () => new Promise<void>(done => {
