d74aed142974f007cff410a1230e723672bbcd52
Samsonium <tdesu@vk.com>
2023-08-21 09:19:46 +0300

    Implement log utils class

---
+++ back/test/logger.spec.ts
@@ -0,0 +1,43 @@
+import {describe, it, vi, expect, afterEach, afterAll} from 'vitest';
+import {existsSync, readdirSync} from 'fs';
+import {resolve} from 'path';
+import Logger from '../src/logger';
+
+describe('Logger util', () => {
+    const logMock = vi.spyOn(console, 'log')
+        .mockImplementation(() => {});
+    let logger: Logger;
+
+    afterEach(() => {
+        logMock.mockClear()
+    });
+
+    afterAll(() => {
+        logMock.mockReset();
+    })
+
+    it('Instantiates without problems', () => {
+        logger = Logger.instance;
+        expect(logger).toBeInstanceOf(Logger);
+    });
+    it('Creates logs dir', () => {
+        expect(existsSync(resolve('./logs'))).toBe(true);
+    });
+    it('Creates log files', () => new Promise<void>(done => {
+        new Promise(r => setTimeout(r, 1000)).then(() => {
+            const dirFiles = readdirSync(resolve('./logs'));
+            expect(dirFiles.length).toBeGreaterThan(0);
+            done();
+        })
+    }));
+    it('Info log works normally', () => {
+        const message = 'Hello, World!';
+        logger.info(message);
+        expect(logMock.mock.lastCall[1]).toBe(message);
+    });
+    it('Warning log works normally', () => {
+        const message = 'Hello, World!';
+        logger.warn(message);
+        expect(logMock.mock.lastCall[1]).toBe(message);
+    });
+});
