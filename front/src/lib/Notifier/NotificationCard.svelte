53f5a618386a6929d291c0a023efdc0ac3dcd683
Samsonium <tdesu@vk.com>
2023-08-24 19:37:26 +0300

    Add notification utils

---
+++ front/src/lib/Notifier/NotificationCard.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+    import {Warning, Check} from 'phosphor-svelte';
+
+    export let id: string = '';
+    export let type: 'success' | 'error' = 'error';
+    export let header = 'Header';
+    export let message = '';
+</script>
+
+<div class="notification-card" {id}>
+    <div class="icon {type}">
+        {#if type === 'success'}
+            <Check weight="bold" color="white" size={24} />
+        {:else}
+            <Warning weight="bold" color="white" size={24} />
+        {/if}
+    </div>
+    <div class="text">
+        <h1>{header}</h1>
+        {#if message}
+            <p>{message}</p>
+        {/if}
+    </div>
+</div>
+
+<style lang="scss">
+    .notification-card {
+      width: 100%;
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: flex-start;
+      background: #2f333a;
+      padding: 8px;
+      border-radius: 8px;
+      margin-top: 8px;
+
+      .text {
+        flex: 1;
+        display: flex;
+        flex-flow: column;
+        align-items: flex-start;
+        padding: 0 16px;
+
+        h1 {
+          font: 700 18px Inter, Roboto, sans-serif;
+          color: white;
+          padding: 0;
+          margin: 0;
+        }
+
+        p {
+          font: 400 14px Inter, Roboto, sans-serif;
+          color: rgba(white, .75);
+          padding: 0;
+          margin: 8px 0 0;
+        }
+      }
+
+      .icon {
+        width: 32px;
+        height: 32px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 4px;
+
+        &.success {
+          background: #7ba82e;
+        }
+
+        &.error {
+          background: #b42c2c;
+        }
+      }
+    }
+</style>
---
+++ front/src/lib/Notifier/Notifier.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import {slide} from 'svelte/transition';
+    import NotificationCard from './NotificationCard.svelte';
+    import Notification from '../../utils/Notification';
+
+    const { store: notifications } = Notification;
+    const animConf = {
+        duration: 150
+    };
+</script>
+
+<div class="notifications-box">
+    {#each $notifications as notification (notification.id)}
+        <div class="notification-card-box" transition:slide={animConf}>
+            <NotificationCard {...notification} />
+        </div>
+    {/each}
+</div>
+
+<style lang="scss">
+    .notifications-box {
+      position: fixed;
+      z-index: 101;
+      bottom: 16px;
+      right: 16px;
+      width: 300px;
+      max-width: calc(100vw - 32px);
+      display: flex;
+      flex-flow: column-reverse;
+      align-items: stretch;
+    }
+</style>
