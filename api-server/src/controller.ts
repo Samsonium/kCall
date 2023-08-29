33690007b444d57d299d12228dbbe914eea70a6f
Samsonium <tdesu@vk.com>
2023-08-29 13:39:25 +0300

    [api]: added controller script

---
+++ api-server/src/controller.ts
@@ -0,0 +1,34 @@
+import logger from './logger';
+import {Server, type Socket} from 'socket.io';
+import type {SocketClientEvents, SocketServerEvents} from 'tools/types/types';
+
+/**
+ * Setup Socket.IO server events
+ * @param server
+ */
+export default function setupEvents(server: Server): void {
+    server.on('connect', (socket: Socket<SocketClientEvents, SocketServerEvents>) => {
+        logger.info('New socket has connected: ' + socket.id);
+
+        // Join room event
+        socket.on('joinRoom', (room, metadata) => {
+            logger.info('Socket ' + socket.id + ' connecting to room ' + room, metadata);
+            // TODO: service.joinRoom
+
+            // User sent message event
+            socket.on('sendMessage', (text) => {
+                // TODO: service.sendMessage
+            });
+
+            // User stream changed event
+            socket.on('changeStream', (type, value) => {
+                // TODO: service.changeStream
+            });
+
+            // User leave room event
+            socket.on('leaveRoom', () => {
+                // TODO: service.leaveRoom
+            });
+        });
+    });
+}
