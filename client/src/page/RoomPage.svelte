0f6f3742e999f272afb969495f85e87f1eca21ba
Samsonium <tdesu@vk.com>
2023-08-30 18:07:03 +0300

    [client]: add room page

---
+++ client/src/page/RoomPage.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let params: Record<string, string> = {};
+
+    console.log(params);
+</script>
+
+<p>Room</p>
+<ul>
+    {#each Object.entries(params) as param}
+        <li>{param[0]} = {param[1]}</li>
+    {/each}
+</ul>
