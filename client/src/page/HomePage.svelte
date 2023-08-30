646741b14f9b3af906f3773ceab136d5e675429d
Samsonium <tdesu@vk.com>
2023-08-30 18:06:53 +0300

    [client]: add home page

---
+++ client/src/page/HomePage.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let pathParams = {};
+    export let queryParams = {};
+
+    console.log(pathParams);
+    console.log(queryParams);
+</script>
+
+<h1>This is home page</h1>
