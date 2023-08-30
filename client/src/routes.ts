b281c458adbce3e87391dc87e264828d6cd2c58b
Samsonium <tdesu@vk.com>
2023-08-30 18:08:05 +0300

    [client]: update routes

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,12 +1,10 @@
 import HomePage from './page/HomePage.svelte';
 import RoomPage from './page/RoomPage.svelte';
-import NotFoundPage from './page/NotFoundPage.svelte';
 import type {SvelteComponent} from 'svelte';
 
 const routes: Record<string, typeof SvelteComponent> = {
     '/': HomePage,
-    '/room/:id': RoomPage,
-    '*': NotFoundPage
+    '/room/:id': RoomPage
 };
 
 export default routes;
