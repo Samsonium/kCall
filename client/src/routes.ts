c284911fe34004c43399e8805681590a71704754
Samsonium <tdesu@vk.com>
2023-08-30 17:52:56 +0300

    [client]: add Router component

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,19 +1,12 @@
 import HomePage from './page/HomePage.svelte';
 import RoomPage from './page/RoomPage.svelte';
 import NotFoundPage from './page/NotFoundPage.svelte';
+import type {SvelteComponent} from 'svelte';
 
-const routes: {}[]= [
-    {
-        name: '/',
-        component: HomePage
-    },
-    {
-        name: '/room/:id',
-        component: RoomPage
-    },
-    {
-        name: '*',
-        component: NotFoundPage
-    }
-];
+const routes: Record<string, typeof SvelteComponent> = {
+    '/': HomePage,
+    '/room/:id': RoomPage,
+    '*': NotFoundPage
+};
+
 export default routes;
