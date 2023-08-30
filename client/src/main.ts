6344860c43adc2abdb3aeb5d44a5464ecfe44a00
Samsonium <tdesu@vk.com>
2023-08-30 18:07:56 +0300

    [client]: app.css renamed to app.scss

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,6 @@
-import './app.css'
-import App from './App.svelte'
+import App from './App.svelte';
+import './app.scss';
 
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
+export default new App({
+  target: document.getElementById('app')!,
+});
