52f1b92b658aa5153fdec2ebddab5e93b5ec3fda
Samsonium <tdesu@vk.com>
2023-09-01 02:39:51 +0300

    [client]: implement chat ui

--- client/src/page/room/Room.svelte
+++ client/src/page/room/Room.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {onMount, onDestroy} from 'svelte';
-    import {fade} from 'svelte/transition';
+    import {fade, slide} from 'svelte/transition';
     import getInvitationLink from '../../utils/getInvitationLink';
     import Meeting from '../../utils/meeting';
     import VideoGrid from './VideoGrid.svelte';
@@ -68,6 +68,21 @@
     let state: Writable<'disconnected' | 'connecting' | 'ready'>;
 
     /**
+     * Chat visibility state
+     */
+    let isChatOpen = false;
+
+    /**
+     * HTML element with scrollable chat
+     */
+    let chatHistory: HTMLDivElement;
+
+    /**
+     * Typing message value
+     */
+    let messageValue = '';
+
+    /**
      * Switch track enable state
      * @param type
      */
@@ -83,6 +98,22 @@
     async function refreshConnection(): Promise<void> {
         await meet?.endCall();
         await meet?.beginCall();
+    }
+
+    /**
+     * Send message to room
+     */
+    function sendMessage(): void {
+        meet?.sendMessage(messageValue);
+        $room.chat = [...$room.chat, {
+            uid: meet.userID,
+            text: messageValue
+        }];
+        messageValue = '';
+        chatHistory?.scrollTo({
+            behavior: 'smooth',
+            top: chatHistory.scrollHeight
+        });
     }
 
     let meet: Meeting;
@@ -114,6 +145,13 @@
         selfVideo.addEventListener('loadedmetadata', () => {
             selfVideo.play();
         }, {once: true});
+    }
+
+    $: if (isChatOpen) {
+        chatHistory?.scrollTo({
+            behavior: 'instant',
+            top: chatHistory.scrollHeight
+        });
     }
 </script>
 
@@ -169,7 +207,8 @@
                         <Screencast size={24} color="white" weight="bold" />
                     </button>
                 </div>
-                <button class="chat">
+                <button class="chat" class:open={isChatOpen}
+                        on:click={() => isChatOpen = !isChatOpen}>
                     <ChatCircle size={24} color="white" weight="bold" />
                 </button>
             </div>
@@ -185,7 +224,7 @@
                 {/if}
                 {#if !streamOptions.tracks.audio && $state === 'ready'}
                     <div class="microphone-off" transition:fade={{duration: 150}}>
-                        <MicrophoneSlash size={24} color="black" weight="bold"/>
+                        <MicrophoneSlash size={24} color="black" weight="bold" />
                         <span>Микрофон выключен</span>
                     </div>
                 {/if}
@@ -197,6 +236,32 @@
                     </div>
                 {/if}
             </div>
+
+            {#if isChatOpen}
+                <div class="chat" transition:slide={{duration:250}}>
+                    <div class="header">
+                        <h3>Чат</h3>
+                        <div class="counter">
+                            <ChatCircle size={16} color="white" weight="bold" />
+                            <span>{$room.chat.length}</span>
+                        </div>
+                    </div>
+                    <div class="chat-history-container" bind:this={chatHistory}>
+                        <div class="chat-history">
+                            {#each $room.chat as message}
+                                <div class="message" class:self={message.uid === meet.userID}>
+                                    <p>{$room.members.find(m => m.uid === message.uid)?.name ?? '???'}</p>
+                                    <div class="content">{message.text}</div>
+                                </div>
+                            {/each}
+                        </div>
+                    </div>
+                    <form on:submit|preventDefault={sendMessage}>
+                        <input required type="text" minLength="1" bind:value={messageValue}>
+                        <button type="submit" class="accent">Отправить</button>
+                    </form>
+                </div>
+            {/if}
         </div>
     </div>
 {/if}
@@ -214,12 +279,150 @@
       flex-flow: column;
       align-items: stretch;
 
+      & > .chat {
+        position: fixed;
+        z-index: 20;
+        bottom: 90px;
+        right: 16px;
+        width: 100%;
+        max-width: 350px;
+        display: flex;
+        flex-flow: column;
+        align-items: stretch;
+        background: var(--panel);
+        border: 1px solid rgba(white, .25);
+        border-radius: var(--round-l);
+        border-bottom-right-radius: 0;
+
+        .header {
+          height: 50px;
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: center;
+          justify-content: space-between;
+          padding: 0 16px;
+          border-bottom: 1px solid rgba(white, .25);
+
+          h3 {
+            font: 700 16px Inter, Roboto, sans-serif;
+            color: white;
+          }
+
+          .counter {
+            display: flex;
+            flex-flow: row nowrap;
+            align-items: center;
+
+            span {
+              margin-left: 4px;
+              color: white;
+              font: 500 16px Inter, Roboto, sans-serif;
+            }
+          }
+        }
+
+        .chat-history-container {
+          max-height: 400px;
+          flex: 1;
+          overflow-x: hidden;
+          overflow-y: auto;
+
+          &::-webkit-scrollbar {
+            width: 10px;
+          }
+
+          &::-webkit-scrollbar-track {
+            background: var(--panel);
+          }
+
+          &::-webkit-scrollbar-thumb {
+            background: rgba(white, .5);
+            border: 2px solid var(--panel);
+            border-radius: 16px;
+          }
+
+          .chat-history {
+            width: 100%;
+            min-height: 200px;
+            display: flex;
+            flex-flow: column;
+            align-items: stretch;
+            justify-content: flex-end;
+
+            .message {
+              display: flex;
+              flex-flow: column;
+              align-items: flex-start;
+              margin-top: 16px;
+              padding: 0 16px;
+
+              .content {
+                max-width: 100%;
+                width: max-content;
+                margin-top: 8px;
+                background: rgba(white, .15);
+                padding: 8px;
+                border-radius: var(--round-s);
+                font: 400 14px Inter, Roboto, sans-serif;
+                color: rgba(white, .75);
+              }
+
+              &.self {
+                align-items: flex-end;
+
+                p {
+                  text-align: right;
+                }
+
+                .content {
+                  background: rgba(white, .25);
+                }
+              }
+            }
+          }
+        }
+
+        form {
+          padding: 16px;
+          margin-top: 8px;
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: stretch;
+          border-top: 1px solid rgba(white, .25);
+
+          input {
+            min-width: 50px;
+            flex: 1;
+            background: rgba(white, .15);
+            border: 1px solid rgba(white, .25);
+            border-radius: var(--round-s);
+            font: 400 14px Inter, Roboto, sans-serif;
+            color: white;
+            padding: 0 12px;
+            transition: border .2s cubic-bezier(.25, 0, 0, 1);
+
+            &:hover {
+              border-color: white;
+            }
+
+            &:focus {
+              border-color: var(--accent);
+            }
+          }
+
+          button {
+            padding: 8px;
+            margin-left: 8px;
+          }
+        }
+      }
+
       .self-video {
         position: fixed;
         z-index: 10;
         left: 16px;
         bottom: 90px;
-        width: 500px;
+        width: 20vw;
         border: 1px solid var(--panel);
         border-radius: var(--round-l);
         overflow: hidden;
@@ -296,7 +499,7 @@
 
         video {
           width: 100%;
-          background: black;
+          background: black !important;
           transform: scaleX(-1);
         }
       }
@@ -433,6 +636,11 @@
           padding: 0;
           background: var(--panel);
           border: 1px solid rgba(white, .5);
+          transition: background .2s cubic-bezier(.25, 0, 0, 1);
+
+          &.open {
+            background: var(--accent);
+          }
         }
       }
     }
