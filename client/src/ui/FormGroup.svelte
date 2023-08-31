c5a576aa3930848c692e4f7faff69432d861527f
Samsonium <tdesu@vk.com>
2023-08-31 14:33:44 +0300

    [client]: finish form group component

--- client/src/ui/FormGroup.svelte
+++ client/src/ui/FormGroup.svelte
@@ -7,8 +7,8 @@
 
 <div class="form-group">
     <label for={id}>{label}</label>
-    <div class="input-line">
-        <input {id} class:no-r-round={$$slots.default} type="text" bind:value>
+    <div class="input-line" class:stacked={$$slots.default}>
+        <input {id} type="text" bind:value>
         <slot/>
     </div>
 </div>
@@ -16,10 +16,11 @@
 <style lang="scss">
     .form-group {
       width: 100%;
+      min-width: 50px;
       display: flex;
       flex-flow: column;
       align-items: stretch;
-      padding-bottom: 16px;
+      padding-bottom: 24px;
 
       label {
         font: 500 14px Inter, Roboto, sans-serif;
@@ -35,10 +36,51 @@
         align-items: stretch;
 
         input {
+          min-width: 50px;
           flex: 1;
           border-radius: var(--round-s);
           background: var(--panel);
           border: 1px solid transparent;
+          color: var(--white);
+          padding: 4px 12px;
+          outline: none;
+          font: 400 16px Inter, Roboto, sans-serif;
+          transition: border .2s cubic-bezier(.25, 0, 0, 1);
+
+          &:hover {
+            border-color: var(--white);
+          }
+
+          &:focus {
+            border-color: var(--accent);
+          }
+        }
+
+        &.stacked {
+          height: 38px;
+          padding: 4px;
+          border-radius: var(--round-s);
+          background: var(--panel);
+          border: 1px solid transparent;
+          transition: border .2s cubic-bezier(.25, 0, 0, 1);
+
+          &:hover {
+            border-color: var(--white);
+          }
+
+          &:focus-within {
+            border-color: var(--accent);
+          }
+
+          input {
+            background: transparent;
+            border: none;
+          }
+
+          :global(& > *:not(input)) {
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+          }
         }
       }
     }
