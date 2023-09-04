03b0554b683046c3257cc1592246d46a95b85cfa
Samsonium <tdesu@vk.com>
2023-09-04 12:36:36 +0300

    (client): update getInvitationLink function logic

--- client/src/utils/getInvitationLink.ts
+++ client/src/utils/getInvitationLink.ts
@@ -4,8 +4,12 @@ import {notify} from './notifier';
  * Generate invitation link and copy
  */
 export default function getInvitationLink(): void {
-    const url = new URL(location.toString());
-    url.hash = '';
+    const resultURL = new URL('https://k-call.ru/');
+
+    const path = decodeURIComponent(location.pathname).split('/').slice(2).join('/');
+    if (path.startsWith('web+kcall://'))
+        resultURL.pathname = '/room/' + path.substring(path.indexOf('://') + 3);
+    else resultURL.pathname = path;
 
     if (!window?.navigator?.clipboard?.writeText) {
         notify.error({
@@ -13,7 +17,7 @@ export default function getInvitationLink(): void {
             message: 'Не удалось скопировать пригласительную ссылку'
         });
     } else {
-        navigator.clipboard.writeText(url.toString()).then(() => notify.success({
+        navigator.clipboard.writeText(resultURL.toString()).then(() => notify.success({
             title: 'Готово!',
             message: 'Ссылка скопирована в буфер'
         }));
