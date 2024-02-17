5a82f56f3d5332145313645c63d7bfc1078befa9
samsonium <tdesu@vk.com>
2024-02-17 22:52:36 +0300

    (#53): re-setup SvelteKit project with static adapter. pnpm dependencies was broken ;(

--- services/frontend/svelte.config.js
+++ services/frontend/svelte.config.js
@@ -3,15 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess({})],
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      strict: true
-    }),
-  },
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			assets: 'build',
+			pages: 'build',
+			fallback: null,
+			strict: true
+		})
+	}
 };
 
 export default config;
